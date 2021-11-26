9acce0e920da42aa0c3a1042b07617e8b1930226
quotzon <quotzon@gmail.com>
2021-11-26 13:59:20 +0300

    Added calloutadv to raw.js

--- src/helpers/raw.js
+++ src/helpers/raw.js
@@ -8,3 +8,19 @@
 module.exports = function(content) {
   return content.fn();
 }
+
+module.exports = function(element) {
+  var inner = element.html();
+  var attrs = getAttrs(element);
+
+  switch (element[0].name) {
+    // <calloutadv>
+    case this.components.calloutadv:
+      var classes = ['callout-inner'];
+      if (element.attr('class')) {
+        classes = classes.concat(element.attr('class').split(' '));
+      }
+
+      return format('<table %s class="calloutadv"><tbody><tr><th class="%s">%s</th><th class="expander"></th></tr></tbody></table>', attrs, classes.join(' '), inner);
+  }
+}
